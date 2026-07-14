efa73f7d0b4a62083927fa063e2443bd037a3617
fdetsch <fdetsch@web.de>
2026-07-14 15:39:50 +0200

    refactor: migrate `calcVar()` (#12)

--- R/calcVar.R
+++ R/calcVar.R
@@ -1,33 +1,70 @@
-#' Calculate space-time variance of a RasterStack or RasterBrick
+methods::setGeneric(
+  "calcVar"
+  , function(x, ...) {
+    standardGeneric("calcVar")
+  }
+)
+
+#' Calculate space-time variance of a raster series
 #' 
-#' @description
-#' The function calculates the (optionally standardised) space-time 
-#' variance of a RasterStack or RasterBrick. 
+#' @description The function calculates the (optionally standardised) space-time 
+#'   variance of a raster series. 
 #' 
-#' @param x a RasterStack or RasterBrick
-#' @param standardised logical. 
-#' @param ... currently not used
+#' @param x A `SpatRaster` (or `Raster*`) series.
+#' @param standardised `logical`, defaults to `FALSE`. 
+#' @param ... For `SpatRaster` input: currently not used. For `Raster*` input: 
+#'   arguments passed to the underlying `SpatRaster` method.
 #' 
-#' @return the mean (optionally standardised) space-time variance.
+#' @return The mean (optionally standardised) space-time variance as `numeric`.
 #' 
 #' @export calcVar
+#' @name calcVar
 #' 
-#' @examples 
-#' data("pacificSST")
+#' @examples
+#' sst = terra::unwrap(pacificSST)
 #' 
-#' calcVar(pacificSST)
-calcVar <- function(x, standardised = FALSE, ...) {
-  
-  if (!standardised) {
-    t <- mean(apply(raster::getValues(x), 1, var, na.rm = TRUE), 
-              na.rm = TRUE)
-    s <- mean(apply(raster::getValues(x), 2, var, na.rm = TRUE), 
-              na.rm = TRUE)
-    vrnc <- t + s
-  } else {
-    vrnc <- var(as.vector(raster::getValues(x)), na.rm = TRUE)
+#' calcVar(sst) # default non-standardised
+#' calcVar(sst, standardised = TRUE)
+
+
+################################################################################
+### function using 'RasterStackBrick' ##########################################
+#' @aliases calcVar,RasterStackBrick-method
+#' @rdname calcVar
+methods::setMethod(
+  "calcVar"
+  , signature(x = "RasterStackBrick")
+  , function(
+    x
+    , ...
+  ) {
+    calcVar(
+      terra::rast(x)
+      , ...
+    )
   }
+)
+
+
+################################################################################
+### function using 'SpatRaster' ################################################
+#' @aliases calcVar,SpatRaster-method
+#' @rdname calcVar
+methods::setMethod(
+  "calcVar"
+  , signature(x = "SpatRaster")
+  , function(x, standardised = FALSE, ...) {
   
-  return(vrnc)
-  
-}
+    if (!standardised) {
+      # compute variance across time and space, leveraging c++ functions for 
+      # speed (see `?terra::app` for details)
+      t <- mean(terra::values(terra::app(x, "sd", na.rm = TRUE)^2)[, 1L])
+      s <- mean((terra::global(x, fun = "sd", na.rm = TRUE)[, 1L])^2)
+      vrnc <- t + s
+    } else {
+      vrnc <- var(as.vector(terra::values(x)), na.rm = TRUE)
+    }
+    
+    return(vrnc)
+  }
+)
---
+++ inst/tinytest/test_calcVar.R
@@ -0,0 +1,36 @@
+library("tinytest")
+library("checkmate")
+using("checkmate")
+
+sst = terra::unwrap(pacificSST)
+
+## non-standardized
+var_ns = calcVar(sst)
+
+expect_number(
+  var_ns
+  , lower = 0
+  , finite = TRUE
+  , info = "returns a single `numeric` value"
+)
+
+## standardized
+var_st = calcVar(sst, standardised = TRUE)
+
+expect_number(
+  var_st
+  , lower = 0
+  , finite = TRUE
+  , info = "returns a single `numeric` value"
+)
+
+expect_true(
+  var_st != var_ns
+)
+
+## `Raster*` input
+expect_identical(
+  calcVar(as(sst, "Raster"))
+  , target = var_ns
+  , info = "returns same result as `Raster*` input"
+)
